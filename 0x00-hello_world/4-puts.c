a6b22940672b17c1a6bf0f707506c195dee85184
Lesego <lesegolmdesign@gmail.com>
2023-10-05 17:42:27 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
