222edab61e91d63dcd6437aca84f581c9774040f
Lesego <lesegolmdesign@gmail.com>
2023-10-12 14:30:10 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Test function for positive or negative
  *
@@ -7,8 +6,9 @@
  */
 int main(void)
 {
-    int i;
-    i = 0;
-    positive_or_negative(i);
-    return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
