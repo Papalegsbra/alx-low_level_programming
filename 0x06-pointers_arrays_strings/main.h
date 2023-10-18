b83bf5541049825c5dda6cd747e50dc7ab19d9d6
Lesego <lesegolmdesign@gmail.com>
2023-10-18 11:46:24 -0700

    coded not checked yet check code

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifdef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,6 +10,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
 
 #endif
