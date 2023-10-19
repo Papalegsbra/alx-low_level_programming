1cff8fdc980aae43d476526ea99e410bdaa161c0
Lesego <lesegolmdesign@gmail.com>
2023-10-19 14:19:29 -0700

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,25 +11,20 @@ char *_strcat(char *dest, char *src)
 {
     int dest_length = 0;
     int src_length = 0;
-
-    // Find the length of dest and src
     while (dest[dest_length] != '\0')
     {
-        dest_length++;
+	    dest_length++;
     }
 
     while (src[src_length] != '\0')
     {
-        src_length++;
+	    src_length++;
     }
 
-    // Copy characters from src to the end of dest
     for (int i = 0; i < src_length; i++)
     {
-        dest[dest_length + i] = src[i];
+	    dest[dest_length + i] = src[i]
     }
-
-    // Add a null terminator at the end of the concatenated string
     dest[dest_length + src_length] = '\0';
 
     return dest;
