f60c9b782dfe1f6a5d737e19f3e75404aa7da445
Lesego <lesegolmdesign@gmail.com>
2023-10-27 22:06:11 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+ /**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string where the concatenation will be stored.
+ * @src: The source string to be concatenated to `dest`.
+ *
+ * Return: A pointer to the destination string (`dest`).
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string to the standard output followed by a newline
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
