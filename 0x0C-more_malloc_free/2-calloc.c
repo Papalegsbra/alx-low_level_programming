e2c8653b0551458bfdd22ff5a658abad421a5db3
Lesego <lesegolmdesign@gmail.com>
2023-11-02 19:06:02 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *custom_memset - fills memory with a cinstant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * * Return: a pointer to memory area s
+ */
+char *custom_memset(char *s, char b, unsigned int n)
+{
+	unsigned  int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory  or NULL on failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
