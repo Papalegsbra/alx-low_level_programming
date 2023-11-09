a538028215b3abf2f139aecbf48540ff1f3d6c85
Lesego <lesegolmdesign@gmail.com>
2023-11-08 17:40:37 -0800

    ememe

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,27 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * main -prints the result of simple operations
- * @argc: the NUmber of arguments supplied to the problem
- * @argv: An array of pointers to the argument
+ * main - prints the result of simple operations
+ * @argc: the number of arguments supplied to the program
+ * @argv: an array of pointers to the arguments
  *
  * Return: Always 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(98);
-	}
 
- 	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	int num1 = atoi(argv[1]);
+	char *op = argv[2];
+	int num2 = atoi(argv[3]);
+	if (op[1] != '\0' || (op[0] != '+' && op[0] != '-' && op[0] != '*' && op[0] != '/' && op[0] != '%'))
 	{
 		printf("Error\n");
 		exit(99);
@@ -48,8 +32,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
+	int result = get_op_func(op)(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
